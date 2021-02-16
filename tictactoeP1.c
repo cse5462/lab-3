9cf99a0597ddbf5f022a6903aaee12efeb0f875c
Conner Graham <59581008+cgraham1092@users.noreply.github.com>
2021-02-16 14:15:00 -0500

    Updated documentation

--- tictactoeP1.c
+++ tictactoeP1.c
@@ -1,8 +1,8 @@
-/**********************************************************/
-/* This program is a 'pass and play' version of tictactoe */
-/* Two users, player 1 and player 2, pass the game back   */
-/* and forth, on a single computer                        */
-/**********************************************************/
+/***********************************************************/
+/* This program is a 'net-enabled' version of tictactoe.   */
+/* Two users, Player 1 and Player 2, send moves back and   */
+/* forth, between two computers.                           */
+/***********************************************************/
 
 /* #include files go here */
 #include <stdio.h>
@@ -19,23 +19,47 @@
 
 /* The number of command line arguments. */
 #define NUM_ARGS 2
-/* TODO */
+/* The maximum length to which the queue of pending connections may grow. */
 #define BACKLOG_MAX 5
-/* #define section, for now we will define the number of rows and columns */
-#define ROWS  3
-#define COLUMNS  3
+/* The number of rows for the TicIacToe board. */
+#define ROWS 3
+/* The number of columns for the TicIacToe board. */
+#define COLUMNS 3
 
-/* C language requires that you predefine all the routines you are writing */
 void print_error(const char *msg, int errnum, int terminate);
 void handle_init_error(const char *msg, int errnum);
 void extract_args(char *argv[], int *port);
 void print_server_info(const struct sockaddr_in *serverAddr);
 int create_endpoint(struct sockaddr_in *socketAddr, unsigned long address, int port);
-int init_shared_state(char board[ROWS][COLUMNS]);
-int tictactoe(int sd, char board[ROWS][COLUMNS]);
+void init_shared_state(char board[ROWS][COLUMNS]);
 int check_win(char board[ROWS][COLUMNS]);
 void print_board(char board[ROWS][COLUMNS]);
+int validate_choice(int choice, char board[ROWS][COLUMNS]);
+int get_p1_choice();
+int get_p2_choice(int sd);
+int get_player_choice(int sd, char board[ROWS][COLUMNS], int player);
+void tictactoe(int sd, char board[ROWS][COLUMNS]);
 
+/**
+ * @brief This program creates and sets up a TicTacToe server which acts as Player 1 in a
+ * 2-player game of TicTacToe. This server creates a server socket for the clients to connect
+ * to, listens for and accepts remote client TCP STREAM connections, and then initiates a simple
+ * game of TicTacToe in which Player 1 and Player 2 take turns making moves which they send to
+ * the other player. If an error occurs before the connection is established, the program
+ * terminates and prints appropriate error messages, otherwise an error message is printed and
+ * the connection to the connected client is terminated.
+ * 
+ * @param argc Non-negative value representing the number of arguments passed to the program
+ * from the environment in which the program is run.
+ * @param argv Pointer to the first element of an array of argc + 1 pointers, of which the
+ * last one is NULL and the previous ones, if any, point to strings that represent the
+ * arguments passed to the program from the host environment. If argv[0] is not a NULL
+ * pointer (or, equivalently, if argc > 0), it points to a string that represents the program
+ * name, which is empty if the program name is not available from the host environment.
+ * @return If the return statement is used, the return value is used as the argument to the
+ * implicit call to exit(). The values zero and EXIT_SUCCESS indicate successful termination,
+ * the value EXIT_FAILURE indicates unsuccessful termination.
+ */
 int main(int argc, char *argv[]) {
     int sd, portNumber;
     char board[ROWS][COLUMNS];
@@ -45,21 +69,25 @@ int main(int argc, char *argv[]) {
 	if (argc != NUM_ARGS) handle_init_error("argc: Invalid number of command line arguments", 0);
 	extract_args(argv, &portNumber);
 
-    /* TODO */
     /* Create server socket from user provided data */
 	sd = create_endpoint(&serverAddr, INADDR_ANY, portNumber);
+
+    /* Print server information and listen for clients wanting to connect */
     if (listen(sd, BACKLOG_MAX) == 0) {
         print_server_info(&serverAddr);
+        /* Play the TicTacToe game whenever a new player connects */
         while (1) {
             int connected_sd;
             struct sockaddr_in clientAddress;
             socklen_t fromLength;
 
             printf("[+]Waiting for Player 2 to join...\n");
+            /* Wait for Player 2 to connect to the game */
             if ((connected_sd = accept(sd, (struct sockaddr *)&clientAddress, &fromLength)) >= 0) {
                 printf("Player 2 connected.\n");
-                init_shared_state(board); // Initialize the 'game' board
-                tictactoe(connected_sd, board); // call the 'game'
+                /* Initialize the 'game' board and start the 'game' */
+                init_shared_state(board);
+                tictactoe(connected_sd, board);
                 printf("[+]The game has ended. Closing the connection.\n");
                 if (close(connected_sd) < 0) print_error("close client-connection", errno, 0);
             } else {
@@ -112,8 +140,8 @@ void handle_init_error(const char *msg, int errnum) {
  * validation on their formatting. If any errors are found, the function terminates the process.
  * 
  * @param argv Pointer to the first element of an array of argc + 1 pointers, of which the
- * last one is null and the previous ones, if any, point to strings that represent the
- * arguments passed to the program from the host environment. If argv[0] is not a null
+ * last one is NULL and the previous ones, if any, point to strings that represent the
+ * arguments passed to the program from the host environment. If argv[0] is not a NULL
  * pointer (or, equivalently, if argc > 0), it points to a string that represents the program
  * name, which is empty if the program name is not available from the host environment.
  * @param port The remote port number that the server should listen on
@@ -127,24 +155,24 @@ void extract_args(char *argv[], int *port) {
 /**
  * @brief TODO
  * 
- * @param serverAddr 
+ * @param serverAddr The socket address structure for the server comminication endpoint.
  */
 void print_server_info(const struct sockaddr_in *serverAddr) {
-    int hostname; 
-    char hostbuffer[256], *IP_addr; 
-    struct hostent *host_entry; 
-  
-    /* To retrieve hostname */
+    int hostname;
+    char hostbuffer[256], *IP_addr;
+    struct hostent *host_entry;
+
+    /* Retrieve the hostname */
     if ((hostname = gethostname(hostbuffer, sizeof(hostbuffer))) == -1) {
         print_error("print_server_info: gethostname", errno, 1);
     }
-    /* To retrieve host information */
+    /* Retrieve the host information */
     if ((host_entry = gethostbyname(hostbuffer)) == NULL) {
         print_error("print_server_info: gethostbyname", errno, 1);
     }
-    /* To convert an Internet network address into ASCII string */
-    IP_addr = inet_ntoa(*((struct in_addr *)host_entry->h_addr_list[0])); 
-  
+    /* Convert the host internet network address to an ASCII string */
+    IP_addr = inet_ntoa(*((struct in_addr *)host_entry->h_addr_list[0]));
+    /* Print the IP address and port number for the server */
     printf("Server listening at %s on port %hu\n", IP_addr, serverAddr->sin_port);
 }
 
@@ -152,10 +180,10 @@ void print_server_info(const struct sockaddr_in *serverAddr) {
  * @brief Creates the comminication endpoint with the provided IP address and port number. If any
  * errors are found, the function terminates the process.
  * 
- * @param socketAddr The socket address structure creates for the comminication endpoint.
+ * @param socketAddr The socket address structure created for the comminication endpoint.
  * @param address The IP address for the socket address structure.
  * @param port The port number for the socket address structure.
- * @param backlog TODO
+ * @param backlog The maximum length to which the queue of pending connections may grow.
  * @return The socket descriptor of the created comminication endpoint.
  */
 int create_endpoint(struct sockaddr_in *socketAddr, unsigned long address, int port) {
@@ -180,20 +208,88 @@ int create_endpoint(struct sockaddr_in *socketAddr, unsigned long address, int p
 	return sd;
 }
 
-int init_shared_state(char board[ROWS][COLUMNS]) {    
-    /* this just initializing the shared state aka the board */
+/**
+ * @brief NOTE
+ * 
+ * @param board The array representing the current state of the game board.
+ */
+void init_shared_state(char board[ROWS][COLUMNS]) {    
     int i, j, count = 1;
-
     printf("[+]Initializing shared game board.\n");
+    /* Initializes the shared state (aka the board)  */
     for (i = 0; i < 3; i++) {
         for (j = 0; j < 3; j++) {
             board[i][j] = count++ + '0';
         }
     }
-    return 0;
 }
 
-int get_p1_choice(int sd) {
+/**
+ * @brief NOTE
+ * 
+ * @param board The array representing the current state of the game board.
+ * @return TODO 
+ */
+int check_win(char board[ROWS][COLUMNS]) {
+    /**************************************************************************/
+    /* Brute force check to see if someone won, or if there is a draw. Return */
+    /* a 0 or 1 if the game is 'over' or return -1 if game should go on       */
+    /**************************************************************************/
+    if (board[0][0] == board[0][1] && board[0][1] == board[0][2]) { // row matches
+        return 1;   // return of 1 mean someone won -> game over
+    } else if (board[1][0] == board[1][1] && board[1][1] == board[1][2]) { // row matches
+        return 1;     
+    } else if (board[2][0] == board[2][1] && board[2][1] == board[2][2]) { // row matches
+        return 1;     
+    } else if (board[0][0] == board[1][0] && board[1][0] == board[2][0]) { // column matches
+        return 1;    
+    } else if (board[0][1] == board[1][1] && board[1][1] == board[2][1]) { // column matches
+        return 1;  
+    } else if (board[0][2] == board[1][2] && board[1][2] == board[2][2]) { // column matches
+        return 1;
+    } else if (board[0][0] == board[1][1] && board[1][1] == board[2][2]) { // diagonal matches
+        return 1;    
+    } else if (board[2][0] == board[1][1] && board[1][1] == board[0][2]) { // diagonal matches
+        return 1;    
+    } else if (board[0][0] != '1' && board[0][1] != '2' && board[0][2] != '3' &&
+                board[1][0] != '4' && board[1][1] != '5' && board[1][2] != '6' && 
+                board[2][0] != '7' && board[2][1] != '8' && board[2][2] != '9') {   // draw
+        return 0;   // return of 0 means draw -> game over
+    } else {
+        return -1;  // return of -1 means keep playing
+    }
+}
+
+/**
+ * @brief NOTE
+ * 
+ * @param board The array representing the current state of the game board.
+ */
+void print_board(char board[ROWS][COLUMNS]) {
+    /*****************************************************************/
+    /* Brute force print out the board and all the squares/values    */
+    /*****************************************************************/
+    /* Print header info */
+    printf("\n\n\n\tCurrent TicTacToe Game\n\n");
+    printf("Player 1 (X)  -  Player 2 (O)\n\n\n");
+    /* Print current state of board */
+    printf("     |     |     \n");
+    printf("  %c  |  %c  |  %c \n", board[0][0], board[0][1], board[0][2]);
+    printf("_____|_____|_____\n");
+    printf("     |     |     \n");
+    printf("  %c  |  %c  |  %c \n", board[1][0], board[1][1], board[1][2]);
+    printf("_____|_____|_____\n");
+    printf("     |     |     \n");
+    printf("  %c  |  %c  |  %c \n", board[2][0], board[2][1], board[2][2]);
+    printf("     |     |     \n\n");
+}
+
+/**
+ * @brief TODO
+ * 
+ * @return TODO 
+ */
+int get_p1_choice() {
     int pick = 0;
     char input[25];
     printf("Player 1, enter a number:  ");
@@ -202,6 +298,12 @@ int get_p1_choice(int sd) {
     return pick;
 }
 
+/**
+ * @brief TODO
+ * 
+ * @param sd The socket descriptor of the connected player's comminication endpoint.
+ * @return TODO 
+ */
 int get_p2_choice(int sd) {
     int rv;
     char pick = '0';
@@ -216,11 +318,18 @@ int get_p2_choice(int sd) {
     return (pick - '0');
 }
 
+/**
+ * @brief TODO
+ * 
+ * @param choice The player move to be validated.
+ * @param board The array representing the current state of the game board.
+ * @return TODO 
+ */
 int validate_choice(int choice, char board[ROWS][COLUMNS]) {
     int row, column;
-    /* TODO */
+    /* Check to see if the choice is a move on the board */
     if (choice < 1 || choice > 9) {
-        print_error("Invalid move: Input must be a number [1-9]", errno, 0);
+        print_error("Invalid move: Must be a number [1-9]", errno, 0);
         return 0;
     }
     /* Check to see if the row/column chosen has a digit in it, if */
@@ -234,8 +343,18 @@ int validate_choice(int choice, char board[ROWS][COLUMNS]) {
     return 1;
 }
 
+/**
+ * @brief TODO
+ * 
+ * @param sd The socket descriptor of the connected player's comminication endpoint.
+ * @param board The array representing the current state of the game board.
+ * @param player The value indicating which player's turn it is.
+ * @return TODO 
+ */
 int get_player_choice(int sd, char board[ROWS][COLUMNS], int player) {
+    /* Get the player's move */
     int choice = (player == 1) ? get_p1_choice(sd) : get_p2_choice(sd);
+    /* Attempt to validate move; reprompt if Player 1, otherwise return error */
     while (!validate_choice(choice, board)) {
         if (player == 1) {
             choice = get_p1_choice(sd);
@@ -243,6 +362,7 @@ int get_player_choice(int sd, char board[ROWS][COLUMNS], int player) {
             return -1;
         }
     }
+    /* If Player 1, we need to send the move to the other player */
     if (player == 1) {
         char pick = choice + '0';
         if (send(sd, &pick, sizeof(char), MSG_NOSIGNAL) < 0) {
@@ -253,8 +373,16 @@ int get_player_choice(int sd, char board[ROWS][COLUMNS], int player) {
     return choice;
 }
 
-int tictactoe(int sd, char board[ROWS][COLUMNS]) {
-    /* this is the meat of the game, you'll look here for how to change it up */
+/**
+ * @brief NOTE
+ * 
+ * @param sd The socket descriptor of the connected player's comminication endpoint.
+ * @param board The array representing the current state of the game board.
+ */
+void tictactoe(int sd, char board[ROWS][COLUMNS]) {
+    /***************************************************************************/
+    /* This is the meat of the game, you'll look here for how to change it up. */
+    /***************************************************************************/
     int player = 1;     // keep track of whose turn it is
     int result, choice; // used for keeping track of choice user makes
     int row, column;
@@ -264,9 +392,9 @@ int tictactoe(int sd, char board[ROWS][COLUMNS]) {
     do {
         /* Print the board on the screen */
         print_board(board);
-        /* TODO */
-        if ((choice = get_player_choice(sd, board, player)) < 0) return 0;
-        /* Depending on who the player is, either use X or O */
+        /* Get the player's move */
+        if ((choice = get_player_choice(sd, board, player)) < 0) return;
+        /* Depending on who the player is, use either X or O */
         mark = (player == 1) ? 'X' : 'O';
         
         /******************************************************************/
@@ -276,12 +404,12 @@ int tictactoe(int sd, char board[ROWS][COLUMNS]) {
         /******************************************************************/
         row = (int)((choice-1) / ROWS); 
         column = (choice-1) % COLUMNS;
-        /* Make move player chose */
+        /* Make the move the player chose */
         board[row][column] = mark;
 
         /* After a move, check to see if someone won! (or if there is a draw) */
         if ((result = check_win(board)) == -1) {
-            /* If not, change turn to other player */
+            /* If not, change to other player's turn */
             player = (player == 1) ? 2 : 1;
         }
     } while (result == -1); // -1 means the game is still going 
@@ -295,61 +423,4 @@ int tictactoe(int sd, char board[ROWS][COLUMNS]) {
     } else {
         printf("==>\a It's a draw\n");   // ran out of squares, it is a draw
     }
-
-    return 1;
-}
-
-int check_win(char board[ROWS][COLUMNS]) {
-    /************************************************************************/
-    /* brute force check to see if someone won, or if there is a draw       */
-    /* return a 0 if the game is 'over' and return -1 if game should go on  */
-    /************************************************************************/
-    if (board[0][0] == board[0][1] && board[0][1] == board[0][2] ) { // row matches
-        return 1;   // return of 1 mean someone won -> game over
-    } else if (board[1][0] == board[1][1] && board[1][1] == board[1][2] ) { // row matches
-        return 1;     
-    } else if (board[2][0] == board[2][1] && board[2][1] == board[2][2] ) { // row matches
-        return 1;     
-    } else if (board[0][0] == board[1][0] && board[1][0] == board[2][0] ) { // column matches
-        return 1;    
-    } else if (board[0][1] == board[1][1] && board[1][1] == board[2][1] ) { // column matches
-        return 1;  
-    } else if (board[0][2] == board[1][2] && board[1][2] == board[2][2] ) { // column matches
-        return 1;
-    } else if (board[0][0] == board[1][1] && board[1][1] == board[2][2] ) { // diagonal matches
-        return 1;    
-    } else if (board[2][0] == board[1][1] && board[1][1] == board[0][2] ) { // diagonal matches
-        return 1;    
-    } else if (board[0][0] != '1' && board[0][1] != '2' && board[0][2] != '3' &&
-                board[1][0] != '4' && board[1][1] != '5' && board[1][2] != '6' && 
-                board[2][0] != '7' && board[2][1] != '8' && board[2][2] != '9') {   // draw
-        return 0;   // return of 0 means draw -> game over
-    } else {
-        return -1;  // return of -1 means keep playing
-    }
-}
-
-
-void print_board(char board[ROWS][COLUMNS]) {
-    /*****************************************************************/
-    /* brute force print out the board and all the squares/values    */
-    /*****************************************************************/
-    printf("\n\n\n\tCurrent TicTacToe Game\n\n");
-
-    printf("Player 1 (X)  -  Player 2 (O)\n\n\n");
-
-    printf("     |     |     \n");
-    printf("  %c  |  %c  |  %c \n", board[0][0], board[0][1], board[0][2]);
-
-    printf("_____|_____|_____\n");
-    printf("     |     |     \n");
-
-    printf("  %c  |  %c  |  %c \n", board[1][0], board[1][1], board[1][2]);
-
-    printf("_____|_____|_____\n");
-    printf("     |     |     \n");
-
-    printf("  %c  |  %c  |  %c \n", board[2][0], board[2][1], board[2][2]);
-
-    printf("     |     |     \n\n");
 }
